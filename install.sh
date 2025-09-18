965e59e38dc4702bff1dcde54f2a92aa42ff7449
Kevin Galassi <k.galassi@robosect.it>
2025-09-18 17:10:37 +0200

    add bas script for install

---
+++ install.sh
@@ -0,0 +1,11 @@
+sudo apt-get install -y ros-humble-moveit \
+    ros-humble-moveit-msgs \
+    ros-humble-usb-cam \
+    ros-humble-ros2-controllers \
+    ros-humble-moveit-servo \
+    ros-humble-ros2-control \
+    ros-humble-moveit-resources
+
+
+sudo apt-get install -y python3-pip
+ install mediapipe
